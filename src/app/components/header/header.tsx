9107e5ba91bd3f025f7db6f31dfae0462abf8152
jhon-crypt <johnoladele690@gmail.com>
2023-09-12 04:24:47 +0100

    Made some chnages

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -1,3 +1,7 @@
+'use client'
+import { FaIdCard } from 'react-icons/fa';
+import { Dropdown } from 'flowbite-react';
+
 export default function Header() {
 
     return (
@@ -8,7 +12,13 @@ export default function Header() {
                 <div className="container mx-auto px-4">
                     <div className="relative flex h-24 items-center">
                         <a className="inline-block" href="#">
-                            <img className="h-10" src="https://shuffle.dev/aurora-assets/logos/aurora-logo-white.svg" alt="" />
+                            <div className="flex items-center">
+
+                                <a href="#" className="flex items-center" style={{ color: "#A3E33A" }}>
+                                    <span className="text-2xl font-bold mr-4"><FaIdCard /></span>
+                                    <span className="text-2xl font-bold font-heading tracking-tight">Nin-Verifier</span>
+                                </a>
+                            </div>
                         </a>
                         <button className="lg:hidden navbar-burger flex items-center justify-center h-10 w-10 ml-auto border border-teal-900 bg-teal-200 bg-opacity-30 hover:bg-teal-700 rounded-lg">
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -20,16 +30,29 @@ export default function Header() {
                         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 hidden lg:flex items-center">
                             <a className="inline-block text-white hover:text-teal-400 mr-10" href="#">
                                 <div className="flex items-center">
-                                    <span className="mr-3">Product</span>
-                                    <svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M1 1L5 5L9 1" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                    </svg>
+                                    <span className="mr-3"><Dropdown
+                                        inline
+                                        label="For who"
+                                    >
+                                        <p className='ms-3 mb-3'>
+                                            Dashboard
+                                        </p>
+                                        <p className='ms-3 mb-3'>
+                                            Settings
+                                        </p>
+                                        <p className='ms-3 mb-3'>
+                                            Earnings
+                                        </p>
+                                        <p className='ms-3'>
+                                            Sign out
+                                        </p>
+                                    </Dropdown></span>
                                 </div>
                             </a>
-                            <a className="inline-block text-white hover:text-teal-400 mr-10" href="#">Features</a><a className="inline-block text-white hover:text-teal-400 mr-10" href="#">Pricing</a><a className="inline-block text-white hover:text-teal-400" href="#">Resources</a>
+                            <a className="inline-block text-white hover:text-teal-400 mr-10" href="#">Use cases</a>
+                            <a className="inline-block text-white hover:text-teal-400" href="#">Contact</a>
                         </div>
                         <div className="hidden lg:block ml-auto">
-                            <a className="inline-flex items-center justify-center h-10 mr-4 px-4 text-center text-sm text-white hover:text-teal-400 font-semibold transition duration-200" href="#">Login</a>
                             <a className="group inline-block justify-center p-1 text-center text-sm text-white font-semibold rounded-lg" href="#">
                                 <div className="inline-flex items-stretch h-10 p-0.5 rounded-lg bg-gradient-to-b from-gray-500 via-gray-700 to-gray-700 hover:to-gray-800">
                                     <div className="flex items-center px-4 bg-gray-700 group-hover:bg-opacity-40 rounded-md transition duration-300">
--- src/app/components/hero/headerHero.tsx
+++ src/app/components/hero/headerHero.tsx
@@ -9,25 +9,14 @@ export default function HeaderHero(){
                         <div className="max-w-2xl mb-14 mx-auto text-center">
                             <a className="group relative inline-flex mb-6 items-center rounded-full overflow-hidden shadow-3xl" href="#">
                                 <div className="animate-showLine absolute bottom-0 left-1/2 transform -translate-x-1/2 p-px mx-auto w-auto bg-gradient-to-r from-transparent via-yellowGreen-500 to-transparent"></div>
-                                <div className="flex flex-wrap sm:flex-nowrap items-center px-6 sm:px-4 py-4 sm:py-2 bg-gray-800 group-hover:bg-opacity-60 rounded-full transition duration-300">
-                                    <span className="text-sm text-white">Get ahead of the game with insights from 1200+ top marketers.</span>
-                                    <div className="mt-1 sm:mt-0 mx-auto sm:mr-0 sm:ml-2 flex items-center">
-                                        <span className="text-transparent bg-clip-text bg-gradient-to-br font-bold from-green-500 via-teal-500 to-cyan-800 transition duration-300">Read more</span>
-                                        <span className="ml-3">
-                                            <svg width="11" height="10" viewBox="0 0 11 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M1.00004 5H9.33337M9.33337 5L5.33337 1M9.33337 5L5.33337 9" stroke="url(#paint0_linear_3112_14890)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                                <defs><linearGradient id="paint0_linear_3112_14890" x1="9.46358" y1="-1.5" x2="5.46163" y2="9.11123" gradientUnits="userSpaceOnUse"><stop stop-color="#ACE348"><stop offset="1" stop-color="#25ACA0"></stop></stop></linearGradient></defs>
-                                            </svg>
-                                        </span>
-                                    </div>
-                                </div>
+                               
                             </a>
                             <div className="max-w-lg sm:max-w-xl lg:max-w-none mx-auto">
-                                <h1 className="font-heading tracking-tight text-4xl sm:text-5xl lg:text-6xl font-bold text-white mb-6">Your marketing assets, at your fingertips.</h1>
-                                <p className="max-w-lg mx-auto text-lg text-gray-300 mb-10">Empowers marketing team to easily store, organize, and access valuable assets from anywhere.</p>
-                                <a className="group relative flex xs:inline-flex items-center justify-center px-5 h-12 font-bold text-gray-900 bg-yellowGreen-600 rounded-lg transition-all duration-300" href="#">
-                                    <div className="absolute top-0 left-0 w-full h-full rounded-lg animate-pulse group-hover:ring-0 transition duration-300" style={{ backgroundColor:"#A3E33A" }}></div>
-                                    <span className="mr-3 text-white">Request Access</span>
+                                <h1 className="font-heading tracking-tight text-4xl sm:text-5xl lg:text-6xl font-bold text-white mb-6">Unlock Possibilities with your Nin Secure Identity Gateway!</h1>
+                                <p className="max-w-lg mx-auto text-lg text-gray-300 mb-10">Experience the ease and security of verifying your identity with our NIN Secure Identity Gateway.</p>
+                                <a className="group relative flex xs:inline-flex items-center justify-center px-5 h-12 font-bold rounded-lg transition-all duration-300" href="#" style={{ backgroundColor:"#A3E33A" }}>
+                                    <div className="absolute top-0 left-0 w-full h-full rounded-lg animate-pulse group-hover:ring-0 transition duration-300" ></div>
+                                    <span className="mr-3 text-dark">Request Access</span>
                                     <span className="transform group-hover:translate-x-1 transition duration-300">
                                         <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M1 1L6 6L1 11" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
@@ -36,8 +25,11 @@ export default function HeaderHero(){
                                 </a>
                             </div>
                         </div>
+                       
+                        {/*}
                         <img className="block mx-auto" src="https://shuffle.dev/aurora-assets/headers/header-dark-theme-center.png" alt=""/>
-                    </div>
+                        {*/}
+                        </div>
                 </div>
         
         </>
