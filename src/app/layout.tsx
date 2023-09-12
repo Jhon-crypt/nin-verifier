8a77ab426b20a20e6b1b13076d7dd15e992a38f1
jhon-crypt <johnoladele690@gmail.com>
2023-09-12 04:38:57 +0100

    Added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
+import Head from 'next/head'
 
 export const metadata: Metadata = {
   title: 'Nin-Verifier',
@@ -13,6 +14,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       <body style={{ backgroundColor: "#041109" }}>{children}</body>
     </html>
   )
